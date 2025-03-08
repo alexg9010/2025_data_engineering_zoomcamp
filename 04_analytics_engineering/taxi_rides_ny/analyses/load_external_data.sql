924aafaa6a5b5ca75d226b81e0e829f2788354a7
Alexander Blume <alex.gos90@gmail.com>
2025-03-08 10:44:35 +0100

    fix: update URIs for external tables in load_external_data.sql to point to correct data sources

--- 04_analytics_engineering/taxi_rides_ny/analyses/load_external_data.sql
+++ 04_analytics_engineering/taxi_rides_ny/analyses/load_external_data.sql
@@ -3,7 +3,7 @@
 CREATE OR REPLACE EXTERNAL TABLE `trips_data_all.green_tripdata`
 OPTIONS (
   format = 'PARQUET',
-  uris = ['https://storage.cloud.google.com/dezoomcamp-dbt--451819-bucket/fhv/fhv_tripdata_2019-*.parquet']
+  uris = ['https://storage.cloud.google.com/dezoomcamp-dbt--451819-bucket/green/green_tripdata_2019-*.parquet']
 );
 
 -- Create external table referring to gcs path
@@ -11,7 +11,7 @@ OPTIONS (
 CREATE OR REPLACE EXTERNAL TABLE `trips_data_all.yellow_tripdata`
 OPTIONS (
   format = 'PARQUET',
-  uris = ['https://storage.cloud.google.com/dezoomcamp-dbt--451819-bucket/fhv/fhv_tripdata_2019-*.parquet']
+  uris = ['https://storage.cloud.google.com/dezoomcamp-dbt--451819-bucket/yellow/yellow_tripdata_2019-*.parquet']
 );
 
 -- Create external table referring to gcs path
@@ -19,5 +19,5 @@ OPTIONS (
 CREATE OR REPLACE EXTERNAL TABLE `trips_data_all.fhv_tripdata`
 OPTIONS (
   format = 'PARQUET',
-  uris = ['https://storage.cloud.google.com/dezoomcamp-dbt--451819-bucket/fhv/fhv_tripdata_2019-']
+  uris = ['https://storage.cloud.google.com/dezoomcamp-dbt--451819-bucket/fhv/fhv_tripdata_2019-*.parquet']
 );
